02cb66f13e2c105a3e51c97297a988926c224107
tanisha <tanishalakhlani@gmail.com>
2023-03-28 11:52:23 +0530

    main functionalities done

---
+++ calorie-calculator-python-code/Fityfeed/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path,include
+from . import views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('',views.home,name='home'),
+    path('user/',views.userPage,name='userPage'), #userPage
+    path('product/',views.fooditem,name='fooditem'),
+    path('createfooditem/',views.createfooditem,name='createfooditem'),
+    path('register/',views.registerPage,name='register'),
+    path('login/',views.loginPage,name='login'),
+    path('logout/',views.logoutUser,name='logout'),
+    path('addFooditem/',views.addFooditem,name='addFooditem'),
+]
